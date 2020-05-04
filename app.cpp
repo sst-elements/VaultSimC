9257a102f11102c632b12c656a4609a2a300b444
sabbirahm3d <sab61r.a@gmail.com>
2020-05-04 16:26:19 -0400

    apply clang tools

--- app.cpp
+++ app.cpp
@@ -13,17 +13,15 @@
 // information, see the LICENSE file in the top level directory of the
 // distribution.
 
-#include <sst/core/sst_config.h>
 #include "cpu.h"
+#include <sst/core/sst_config.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 
-
-static unsigned int missRate[][3] = {{0,51,32},   //app 0
-				     {0,18,15}};  //app 1
-static unsigned int isLoad[] = {3,32}; // out of 64
+static unsigned int missRate[][3] = {{0, 51, 32},  // app 0
+                                     {0, 18, 15}}; // app 1
+static unsigned int isLoad[] = {3, 32};            // out of 64
 
 using namespace SST::MemHierarchy;
 using namespace SST::VaultSim;
@@ -35,11 +33,11 @@ l/s ratio	21:1	1:1
 L1 miss/1Kinst  51	18
 L2 miss/inst	32	15
   */
-  //printf("using missrate %d\n", missRate[app][cacheLevel]);
+  // printf("using missrate %d\n", missRate[app][cacheLevel]);
 
   unsigned int roll1K = rng->generateNextUInt32() & 0x3ff;
   if (roll1K <= missRate[app][cacheLevel]) {
-    //is a memory access
+    // is a memory access
     unsigned int roll = rng->generateNextUInt32();
     unsigned int memRoll = roll & 0x3f;
 
@@ -49,21 +47,21 @@ L2 miss/inst	32	15
       addr = coreAddr[core] + (1 << 6);
       addr = (addr >> 6) << 6;
     } else {
-      //random
+      // random
       addr = (roll >> 6) << 6;
     }
     coreAddr[core] = addr;
-	bool isWrite;
+    bool isWrite;
 
     if (memRoll <= isLoad[app]) {
-        isWrite = false;
+      isWrite = false;
     } else {
-        isWrite = true;
+      isWrite = true;
     }
-    MemReqEvent *event = new MemReqEvent((ReqId)this, addr, isWrite, 0, 0 );
+    auto *event = new MemReqEvent((ReqId)this, addr, isWrite, 0, 0);
 
     return event;
   } else {
-    return 0;
+    return nullptr;
   }
 }
--- cpu.h
+++ cpu.h
@@ -13,15 +13,13 @@
 // information, see the LICENSE file in the top level directory of the
 // distribution.
 
-
 #ifndef _CPU_H
 #define _CPU_H
 
-#include <sst/core/component.h>
 #include "memReqEvent.h"
-#include <sst/core/rng/sstrng.h>
+#include <sst/core/component.h>
 #include <sst/core/output.h>
-
+#include <sst/core/rng/sstrng.h>
 
 using namespace std;
 
@@ -37,43 +35,39 @@ namespace VaultSim {
 class cpu : public Component {
 
 public: // functions
+  SST_ELI_REGISTER_COMPONENT(cpu, "VaultSimC", "cpu",
+                             SST_ELI_ELEMENT_VERSION(1, 0, 0),
+                             "A simple 'cpu' ", COMPONENT_CATEGORY_PROCESSOR)
 
-    SST_ELI_REGISTER_COMPONENT(
-                               cpu,
-                               "VaultSimC",
-                               "cpu",
-                               SST_ELI_ELEMENT_VERSION(1,0,0),
-                               "A simple 'cpu' ",
-                               COMPONENT_CATEGORY_PROCESSOR)
+  SST_ELI_DOCUMENT_PARAMS(
+      {"clock", "Simple CPU Clock Rate."},
+      {"threads", "Number of simulated threads in cpu."},
+      {"app", "Synthetic Application. 0:miniMD-like 1:phdMesh-like. (See "
+              "app.cpp for details)."},
+      {"bwlimit", "Maximum number of memory instructions issued by the "
+                  "processor per cycle. Note, each thread can only have at "
+                  "most 2 outstanding memory references at a time. "},
+      {"seed", "Optional random number generator seed. If not defined or 0, "
+               "uses srandomdev()."})
 
-    SST_ELI_DOCUMENT_PARAMS(
-                            {"clock",              "Simple CPU Clock Rate."},
-                            {"threads",            "Number of simulated threads in cpu."},
-                            {"app",                "Synthetic Application. 0:miniMD-like 1:phdMesh-like. (See app.cpp for details)."},
-                            {"bwlimit",            "Maximum number of memory instructions issued by the processor per cycle. Note, each thread can only have at most 2 outstanding memory references at a time. "},
-                            {"seed",               "Optional random number generator seed. If not defined or 0, uses srandomdev()."}
-                            )
+  SST_ELI_DOCUMENT_PORTS({"toMem",
+                          "Link to the memory system",
+                          {"memEvent", ""}})
 
-    SST_ELI_DOCUMENT_PORTS(
-                           {"toMem", "Link to the memory system", {"memEvent",""}}
-                           )
-
-  cpu( ComponentId_t id, Params& params );
-  void finish();
+  cpu(ComponentId_t id, Params &params);
+  void finish() override;
 
 private: // types
-
   typedef SST::Link memChan_t;
-  typedef set<uint64_t> memSet_t;
-  typedef vector<memSet_t> thrSet_t;
-  typedef vector<int> coreVec_t;
+  using memSet_t = set<uint64_t>;
+  using thrSet_t = vector<memSet_t>;
+  using coreVec_t = vector<int>;
 
 private:
+  cpu(const cpu &c);
+  bool clock(Cycle_t);
 
-  cpu( const cpu& c );
-  bool clock( Cycle_t );
-
-  SST::RNG::SSTRandom* rng;
+  SST::RNG::SSTRandom *rng;
   memChan_t *toMem;
   unsigned int outstanding;
   unsigned long long memOps;
@@ -90,7 +84,7 @@ protected:
   Output &out;
 };
 
-}
-}
+} // namespace VaultSim
+} // namespace SST
 
 #endif
